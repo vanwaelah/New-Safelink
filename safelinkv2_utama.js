507f09de039dd83a8a61870a4682c700d31b207a
vanwaelah <42832483+vanwaelah@users.noreply.github.com>
2019-09-08 05:30:59 +0700

    Update safelinkv2_utama.js

--- safelinkv2_utama.js
+++ safelinkv2_utama.js
@@ -21,11 +21,11 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="forex-traders-falling.html"}
-if(x=="2"){xxx="forex-trading-ifx-markets-review.html"}
-if(x=="3"){xxx="how-to-build-winning-trading-plan.html"}
-if(x=="4"){xxx="how-to-choose-best-forex-robot.html"}
-if(x=="5"){xxx="learn-to-listen-to-forex-market.html"}a_to_ve[i].href="https://resepenakmasakandanminuman.blogspot.com/2019/09/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="09/forex-traders-falling.html"}
+if(x=="2"){xxx="09/forex-trading-ifx-markets-review.html"}
+if(x=="3"){xxx="09/how-to-build-winning-trading-plan.html"}
+if(x=="4"){xxx="09/how-to-choose-best-forex-robot.html"}
+if(x=="5"){xxx="09/learn-to-listen-to-forex-market.html"}a_to_ve[i].href="https://resepenakmasakandanminuman.blogspot.com/2019/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
